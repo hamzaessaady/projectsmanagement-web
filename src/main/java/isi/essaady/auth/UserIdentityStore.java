ea0a9ae9a8efbb6a21e391f9a78dfb8bbad483ba
Kuohamkun <50623506+Kuohamkun@users.noreply.github.com>
2020-05-25 14:42:48 +0100

    [F/Login] Complete login process with Security layer

---
+++ src/main/java/isi/essaady/auth/UserIdentityStore.java
@@ -0,0 +1,46 @@
+package isi.essaady.auth;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Optional;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.ApplicationScoped;
+import javax.security.enterprise.credential.Credential;
+import javax.security.enterprise.credential.UsernamePasswordCredential;
+import javax.security.enterprise.identitystore.CredentialValidationResult;
+import javax.security.enterprise.identitystore.IdentityStore;
+
+import isi.essaady.ejbs.UserBean;
+import isi.essaady.entities.User;
+
+/*
+ * Resource : The Definitive Guide to JSF in Java EE 8 by Bauke Scholtz, Arjan Tijms.
+ * [MODIFIED]
+ * 
+ * */
+@ApplicationScoped
+public class UserIdentityStore implements IdentityStore {
+	
+	@EJB
+	private UserBean userBean;
+	
+	@Override
+	public CredentialValidationResult validate(Credential credential) {
+		UsernamePasswordCredential login = (UsernamePasswordCredential) credential;
+		String username = login.getCaller();
+		String password = login.getPasswordAsString();
+		Optional<User> optionalUser = userBean.findByUsernamePassword(username, password);
+		if (optionalUser.isPresent()) {
+			User user = optionalUser.get();
+			System.out.print(user.getUserName() + " " + user.getPassword());
+			return new CredentialValidationResult(
+					user.getUserName(),
+					new LinkedHashSet<String>(
+							Arrays.asList(user.getUserGroup(),user.getRole())
+					));
+		} else {
+			return CredentialValidationResult.INVALID_RESULT;
+		}
+	}
+}
